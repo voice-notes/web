33b0d400ee9249a70c94d29133c8fd085805d1fd
Ben Galley <bengalley21@gmail.com>
2020-07-21 11:54:44 +0200

    add options bar component and test it renders

--- src/components/OptionBar/optionBar.test.tsx
+++ src/components/OptionBar/optionBar.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
+import { OptionBar } from './optionBar'
 // import { Header } from './header';
 
 describe('Options bar', () => {
   it('renders record button', () => {
-    const wrapper = shallow(<OptionBar/>);
-    expect(wrapper.find('button')).toHaveTextContent('Start recording')
+    const wrapper = shallow(<OptionBar />);
+    expect(wrapper.find('button').text()).toEqual('Start recording')
   })
 })
---
+++ src/components/OptionBar/optionBar.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+export class OptionBar extends React.Component {
+  render() {
+    return(
+      <div>
+        <button>Start recording</button>
+      </div>
+    )
+  }
+}
