517628484aed6834203876df5c0d44854c979cfd
Liz Daly <liz7daly@gmail.com>
2020-07-21 10:18:44 +0100

    Add snapshot test

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
-import { shallow, render } from 'enzyme';
+import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import { Header } from './header';
+
+
+it('renders correctly', () => {
+    const container = render(<Header img="test">Test</Header>)
+    expect(container).toMatchSnapshot();
+});
 
 describe('Header', () => {
   test('renders title', () => {
