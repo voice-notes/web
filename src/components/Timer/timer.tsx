a547d2a6bef1eafcfd341a41983baa23e8d99ac5
Ben Galley <bengalley21@gmail.com>
2020-07-22 12:00:30 +0200

    working on timer

--- src/components/Timer/timer.tsx
+++ src/components/Timer/timer.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import Timer from 'react-compound-timer';
+import { start } from 'repl';
 
 interface TimerProps {
-  isRecording: boolean
+  isRecording: boolean;
+}
+
+interface TimerMethods {
+  start: () => void;
+  stop: () => void; 
+  reset: () => void; 
 }
 
 export const RecordingTimer: React.FunctionComponent<TimerProps> = (
     props: TimerProps
   ) => {
+    timerController(start: () => void, stop: void, reset: void) {
+      if (this.props.isRecording) { start() } 
+    }
   return (
-    <Timer>
-      <Timer.Minutes />:
-      <Timer.Seconds />
+    <Timer
+      startImmediately={false} >
+        {({ start, stop, reset }: TimerMethods) => (
+        <React.Fragment>  
+          <Timer.Minutes />:
+          <Timer.Seconds />   
+          <button onClick={start}>Start</button>
+          <button onClick={stop}>Stop</button>
+          <button onClick={reset}>Reset</button>
+        </React.Fragment>  
+      )}  
     </Timer>
   )
 } 
+
+
