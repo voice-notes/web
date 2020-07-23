23317314513bef96f9bdf37ece0fb200f8fbdcb3
Liz Daly <liz7daly@gmail.com>
2020-07-23 16:19:19 +0100

    Rename mock files to ts

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'preset-typescript',
   testEnvironment: 'node',
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.ts"
   },
 };
