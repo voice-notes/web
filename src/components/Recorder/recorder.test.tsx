e42389e4715f8461d2e38b91f2793e6d0c920c47
Liz Daly <liz7daly@gmail.com>
2020-07-23 16:27:09 +0100

    Update tests to include setup in dividual tests

--- src/components/Recorder/recorder.test.tsx
+++ src/components/Recorder/recorder.test.tsx
@@ -10,32 +10,31 @@ jest.mock('react-mic', () => {
   };
 });
 describe('Recorder', () => {
-  let wrapper: any;
-
-  beforeEach(() => {
-    wrapper = shallow(<Recorder />);
-  });
-
+  
   it('renders record button', () => {
     const wrapper = shallow(<Recorder />);
     expect(wrapper.find('button').text()).toEqual('Start recording');
   });
 
   it('updates the button text', () => {
+    const wrapper = shallow(<Recorder />);
     wrapper.find('#record').simulate('click');
     expect(wrapper.find('button').text()).toEqual('Stop recording');
   });
 
   it('is not recording by default', () => {
+    const wrapper = shallow(<Recorder />);
     expect(wrapper.state('isRecording')).toEqual(false);
   });
 
   it('toggles the state of a recording', () => {
+    const wrapper = shallow(<Recorder />);
     wrapper.find('#record').simulate('click');
     expect(wrapper.state('isRecording')).toEqual(true);
   });
 
   it('stops a recording', () => {
+    const wrapper = shallow(<Recorder />);
     wrapper.find('#record').simulate('click');
     wrapper.find('#record').simulate('click');
     expect(wrapper.state('isRecording')).toEqual(false);
