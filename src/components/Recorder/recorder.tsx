52b12dc1ad5b95e360659b954e3e3341edc20999
Liz Daly <liz7daly@gmail.com>
2020-07-30 15:38:34 +0100

    Refactor class to functional component

--- src/components/Recorder/recorder.tsx
+++ src/components/Recorder/recorder.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent } from 'react';
 import { RecordingStatus } from '../RecordingStatus/recordingStatus';
 import { ReactMic } from 'react-mic';
 import { RecordingTimer } from '../Timer/timer';
@@ -8,31 +8,28 @@ interface RecorderProps {
   isRecording: boolean;
 }
 
-export class Recorder extends Component<RecorderProps> {
+export const Recorder:FunctionComponent<RecorderProps> = ({isRecording}:RecorderProps) => {
 
-  onData(recordedBlob: any) {
+  function onData(recordedBlob: any) {
     console.log('chunk of real-time data is: ', recordedBlob);
   }
 
-  onStop(recordedBlob: any) {
+  function onStop(recordedBlob: any) {
     console.log('recordedBlob is: ', recordedBlob);
   }
-
-  render() {
-    const { isRecording } = this.props;
-    return (
-      <div className={styles.container}>
-        <RecordingStatus isRecording={isRecording} />
-        <ReactMic
-          record={isRecording}
-            className="sound-wave"
-            onStop={this.onStop}
-            onData={this.onData}
-            strokeColor="#000000"
-            backgroundColor="#0A9E74"
-        />
-        <RecordingTimer isRecording={isRecording} />
-      </div> 
-    );
-  }
+  
+  return (
+    <div className={styles.container}>
+      <RecordingStatus isRecording={isRecording} />
+      <ReactMic
+        record={isRecording}
+          className="sound-wave"
+          onStop={onStop}
+          onData={onData}
+          strokeColor="#000000"
+          backgroundColor="#0A9E74"
+      />
+      <RecordingTimer isRecording={isRecording} />
+    </div> 
+  );
 }
