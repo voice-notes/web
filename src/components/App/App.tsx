6876d9fd2253e0c17897e1f5e7752686b5531188
Ben Galley <bengalley21@gmail.com>
2020-07-30 13:15:00 +0100

    move recording button and state up to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,37 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from '../Header/header';
 import { Recorder } from '../Recorder/recorder';
 import styles from './App.module.css';
+import { RecordingIcon } from '../RecordingIcon/recordingIcon';
 
 export const App = () => {
+  const [isRecording, setIsRecording] = useState(false); 
+  const [buttonText, setButtonText] = useState('Start');
+
+  function toggleRecording() {
+    setIsRecording(!isRecording);
+  }
+
+  useEffect(() => {
+    if(isRecording) {
+      setButtonText('Start');
+      return;
+    }
+    setButtonText('Stop');
+  })
+
   return (
     <div className={styles.app}>
       <Header>Taped It</Header>
-      <Recorder />
+      <button
+        className={styles.btn}
+        id="record"
+        onClick={() => toggleRecording()}
+      >
+        <RecordingIcon isRecording={isRecording} />
+        <span className={styles.btnTxt}>{buttonText} recording</span>
+      </button>
+      <Recorder isRecording={isRecording} />
     </div>
   );
 };
--- src/components/Recorder/recorder.tsx
+++ src/components/Recorder/recorder.tsx
@@ -2,29 +2,13 @@ import React, { Component } from 'react';
 import { RecordingStatus } from '../RecordingStatus/recordingStatus';
 import { ReactMic } from 'react-mic';
 import { RecordingTimer } from '../Timer/timer';
-import { RecordingIcon } from '../RecordingIcon/recordingIcon';
 import styles from './recorder.module.css';
 
-type recorderState = {
+interface RecorderProps {
   isRecording: boolean;
-  buttonText: string;
-};
+}
 
-export class Recorder extends Component<{}, recorderState> {
-  constructor(props: {}, state: recorderState) {
-    super(props, state);
-    this.state = {
-      isRecording: false,
-      buttonText: 'Start',
-    };
-  }
-  toggleRecording() {
-    if (this.state.isRecording) {
-      this.setState({ isRecording: false, buttonText: 'Start' });
-      return;
-    }
-    this.setState({ isRecording: true, buttonText: 'Stop' });
-  }
+export class Recorder extends Component<RecorderProps> {
 
   onData(recordedBlob: any) {
     console.log('chunk of real-time data is: ', recordedBlob);
@@ -35,31 +19,20 @@ export class Recorder extends Component<{}, recorderState> {
   }
 
   render() {
-    const { buttonText, isRecording } = this.state;
-
+    const { isRecording } = this.props;
     return (
-      <div>
-        <button
-          className={styles.btn}
-          id="record"
-          onClick={() => this.toggleRecording()}
-        >
-          <RecordingIcon isRecording={isRecording} />
-          <span className={styles.btnTxt}>{buttonText} recording</span>
-        </button>
-        <div className={styles.container}>
-          <RecordingStatus isRecording={isRecording} />
-          <ReactMic
-            record={isRecording}
+      <div className={styles.container}>
+        <RecordingStatus isRecording={isRecording} />
+        <ReactMic
+          record={isRecording}
             className="sound-wave"
             onStop={this.onStop}
             onData={this.onData}
             strokeColor="#000000"
             backgroundColor="#0A9E74"
-          />
-          <RecordingTimer isRecording={isRecording} />
-        </div>
-      </div>
+        />
+        <RecordingTimer isRecording={isRecording} />
+      </div> 
     );
   }
 }
