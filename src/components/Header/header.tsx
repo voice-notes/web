d980755954835779256061d25f089cec3a080105
Liz Daly <liz7daly@gmail.com>
2020-08-04 15:50:31 +0100

    Add Title component into Header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { Title } from '../Title/title';
 import { RecordingStatus } from '../RecordingStatus/recordingStatus';
 import styles from './header.module.css';
 import { ReactComponent as TapeLogo } from '../../assets/tape.svg';
@@ -8,12 +9,11 @@ interface Props {
 }
 
 export const Header: FunctionComponent<Props> = ({ isRecording }: Props) => {
-  const title = 'Taped It';
   return (
     <div className={styles.header}>
       <TapeLogo className={styles.logo} title="Tape It logo" />
       <div className={styles.title}>
-        <h1>{title}</h1>
+        <Title isRecording={isRecording} />
         <RecordingStatus isRecording={isRecording} />
       </div>
     </div>
