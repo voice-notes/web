cec6c598c426641c9c34de3c68f90913e9d974b4
Joanne Young <joanneyoung_@hotmail.com>
2020-08-04 16:09:23 +0100

    Add styles to title component

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './title.module.css';
 
 interface Props {
   isRecording: boolean;
 }
 
-export const Title = (props: Props) => {
-  if (props.isRecording) return <h1>Taping It</h1>;
-  return <h1>Tape It</h1>;
+export const Title = ({ isRecording }: Props) => {
+  const text = isRecording ? 'Taping It' : 'Tape It';
+  return <h1 className={styles.title}>{text}</h1>;
 };
