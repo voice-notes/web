4f50c157fe04cefa72e65b3cbca70b7f44121b52
Joanne Young <joanneyoung_@hotmail.com>
2020-08-04 15:56:17 +0100

    Fix failing header component test

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,16 +1,8 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import { Header } from './header';
 
 it('renders correctly', () => {
-  const container = render(<Header>Test</Header>);
+  const container = render(<Header isRecording={false} />);
   expect(container).toMatchSnapshot();
-});
-
-describe('Header', () => {
-  test('renders title', () => {
-    const wrapper = shallow(<Header>Taped It</Header>);
-    expect(wrapper.find('h1').text()).toEqual('Taped It');
-  });
 });
