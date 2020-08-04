c967fa293b714ab3a92ed8bd3771f01405836325
Joanne Young <joanneyoung_@hotmail.com>
2020-08-04 15:14:59 +0100

    Create title component

--- src/components/Title/title.test.tsx
+++ src/components/Title/title.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import { Title } from './title';
 
 describe('Title', () => {
-  it('displays "Tape It" when not recording', ()=> {
+  it('displays "Tape It" when not recording', () => {
     const wrapper = shallow(<Title isRecording={false} />);
-    expect(wrapper.text()).toEqual('Tape It')
-  })
-})
+    expect(wrapper.text()).toEqual('Tape It');
+  });
+});
---
+++ src/components/Title/title.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  isRecording: boolean;
+}
+
+export const Title = (props) => {
+  return <div>Tape It</div>
+}
