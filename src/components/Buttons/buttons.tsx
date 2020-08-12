81c7ad0a60faa3e91c5fa47513be23c4a5920232
Liz Daly <liz7daly@gmail.com>
2020-08-12 13:04:58 +0100

    Remove Function Component type definition

--- src/components/Buttons/buttons.tsx
+++ src/components/Buttons/buttons.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import styles from './buttons.module.css';
 import { IconContext } from 'react-icons';
 import { TiMediaRecordOutline, TiMediaStopOutline } from 'react-icons/ti';
@@ -7,16 +7,12 @@ interface Props {
   isRecording: boolean;
 }
 
-export const RecordingButtonText: FunctionComponent<Props> = ({
-  isRecording,
-}: Props) => {
+export const RecordingButtonText = ({ isRecording }: Props) => {
   const mediaAction = isRecording ? 'Stop' : 'Start';
   return <span className={styles.text}>{mediaAction} recording</span>;
 };
 
-export const RecordingButtonIcon: FunctionComponent<Props> = ({
-  isRecording,
-}: Props) => {
+export const RecordingButtonIcon = ({ isRecording }: Props) => {
   const iconComponent = isRecording ? (
     <TiMediaStopOutline />
   ) : (
