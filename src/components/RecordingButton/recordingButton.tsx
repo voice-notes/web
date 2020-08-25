892b9095716dd2b920dc2bc8881d2121b0726561
Joanne Young <joanneyoung_@hotmail.com>
2020-08-25 13:55:14 +0100

    Re-record message showing on button

--- src/components/RecordingButton/recordingButton.tsx
+++ src/components/RecordingButton/recordingButton.tsx
@@ -5,31 +5,37 @@ import { TiMediaRecordOutline, TiMediaStopOutline } from 'react-icons/ti';
 
 interface Props {
   isRecording: boolean;
+  status: string;
 }
 
 interface ButtonProps {
   onClickRecord: () => void;
   isRecording: boolean;
+  status: string;
 }
 
 export const RecordingButton = ({
   onClickRecord,
   isRecording,
+  status,
 }: ButtonProps) => {
   return (
     <button className={styles.btn} id="record" onClick={onClickRecord}>
-      <RecordingButtonIcon isRecording={isRecording} />
-      <RecordingButtonText isRecording={isRecording} />
+      <RecordingButtonIcon isRecording={isRecording} status={status} />
+      <RecordingButtonText isRecording={isRecording} status={status} />
     </button>
   );
 };
 
-export const RecordingButtonText = ({ isRecording }: Props) => {
+export const RecordingButtonText = ({ isRecording, status }: Props) => {
   const mediaAction = isRecording ? 'Stop' : 'Start';
+  if (status === 'recorded') {
+    return <span className={styles.text}>Re-record</span>;
+  }
   return <span className={styles.text}>{mediaAction} recording</span>;
 };
 
-export const RecordingButtonIcon = ({ isRecording }: Props) => {
+export const RecordingButtonIcon = ({ isRecording}: Props) => {
   const iconComponent = isRecording ? (
     <TiMediaStopOutline />
   ) : (
