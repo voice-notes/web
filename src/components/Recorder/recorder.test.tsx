b255869eceafaae560d95859513139da5da589df
Joanne Young <joanneyoung_@hotmail.com>
2020-08-25 15:38:12 +0100

    Update rerecord test

--- src/components/Recorder/recorder.test.tsx
+++ src/components/Recorder/recorder.test.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import { Recorder } from './recorder';
-import { Timer } from '../Timer/timer';
 
 describe('Recorder', () => {
   it('renders App', () => {
