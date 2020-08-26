bf7cb15b58f64384342325d5c976ce3f427ca2d7
Joanne Young <joanneyoung_@hotmail.com>
2020-08-26 16:44:26 +0100

    Change react icon, adjust padding on icon

--- src/components/SendButton/sendButton.tsx
+++ src/components/SendButton/sendButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './sendButton.module.css';
 import { IconContext } from 'react-icons';
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { FiArrowRight } from 'react-icons/fi';
 
 interface Props {
   status: string;
@@ -12,7 +12,7 @@ export const SendButton = ({ status }: Props) => {
     return (
       <button className={styles.btn} id="send">
         <IconContext.Provider value={{ className: styles.icons }}>
-          <AiOutlineArrowRight />
+          <FiArrowRight />
         </IconContext.Provider>
         <span className={styles.text}>send</span>
       </button>
