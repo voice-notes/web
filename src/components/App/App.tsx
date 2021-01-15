f66931a276571f9dc4f34ccefb2aa401c61e011b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-15 11:14:58 +0000

    Able to store query string as an object in state ready for voice note submit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
-import { Header } from '../Header/header';
-import { Recorder } from '../Recorder/recorder';
-import { RecordingButton } from '../RecordingButton/recordingButton';
-import styles from './App.module.css';
+import React, { useState, useEffect } from "react";
+import { Header } from "../Header/header";
+import { Recorder } from "../Recorder/recorder";
+import { RecordingButton } from "../RecordingButton/recordingButton";
+import styles from "./App.module.css";
+import queryString from "query-string";
 
 export const App = () => {
   const [isRecording, setIsRecording] = useState(false);
+  const [queryParams, setQueryParams] = useState({});
+
+  useEffect(() => {
+    const params = window.location.search;
+    const parsedParams = queryString.parse(params);
+    setQueryParams(parsedParams);
+  }, []);
 
   function toggleRecording() {
     setIsRecording(!isRecording);
