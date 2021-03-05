b9186764ccc38b104e4f448d93d4a6914ccf9108
Tim <timcpb@hotmail.com>
2021-03-05 12:08:53 +0000

    update isRecording to currentRecordingStatus

--- src/components/Title/title.test.tsx
+++ src/components/Title/title.test.tsx
@@ -1,15 +1,16 @@
-import * as React from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
+
 import { Title } from './title';
 
 describe('Title', () => {
   it('displays "Tape It" when not recording', () => {
-    const wrapper = shallow(<Title isRecording={false} />);
+    const wrapper = shallow(<Title currentRecordingStatus={'ready'} />);
     expect(wrapper.text()).toEqual('Tape It');
   });
 
   it('displays "Taping It" when recording', () => {
-    const wrapper = shallow(<Title isRecording={true} />);
+    const wrapper = shallow(<Title currentRecordingStatus={'recording'} />);
     expect(wrapper.text()).toEqual('Taping It');
   });
 });
