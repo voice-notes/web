d1699520559ec856800ad7a09b34d44c67a159c3
Tim <timcpb@hotmail.com>
2021-03-05 15:33:30 +0000

    Add slackId & responseUrl props to component render

--- src/components/SendButton/sendButton.test.tsx
+++ src/components/SendButton/sendButton.test.tsx
@@ -4,18 +4,34 @@ import { SendButton } from './sendButton';
 
 describe('Button', () => {
   it('displays send button after recording', () => {
-    const wrapper = shallow(<SendButton currentRecordingStatus={'recorded'} />);
+    const wrapper = shallow(
+      <SendButton
+        currentRecordingStatus={'recorded'}
+        slackId={''}
+        responseUrl={''}
+      />
+    );
     expect(wrapper.text()).toContain('send');
   });
 
   it('does not display send button before recording', () => {
-    const wrapper = shallow(<SendButton currentRecordingStatus={'ready'} />);
+    const wrapper = shallow(
+      <SendButton
+        currentRecordingStatus={'ready'}
+        slackId={''}
+        responseUrl={''}
+      />
+    );
     expect(wrapper.text()).not.toContain('send');
   });
 
   it('does not display send button during recording', () => {
     const wrapper = shallow(
-      <SendButton currentRecordingStatus={'recording'} />
+      <SendButton
+        currentRecordingStatus={'recording'}
+        slackId={''}
+        responseUrl={''}
+      />
     );
     expect(wrapper.text()).not.toContain('send');
   });
