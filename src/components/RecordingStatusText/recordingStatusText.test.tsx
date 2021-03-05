fe1344a804d3755a0ac5d897ead2cea606e672e6
Tim <timcpb@hotmail.com>
2021-03-05 12:07:08 +0000

    rename recordingStatus component as recordingStatusText

---
+++ src/components/RecordingStatusText/recordingStatusText.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { RecordingStatusText } from './recordingStatusText';
+
+describe('RecordingStatusText', () => {
+  it('renders the current recording status', () => {
+    const wrapper = shallow(<RecordingStatusText currentRecordingStatus={'ready'} />);
+    expect(wrapper.find('.status').text()).toEqual('Ready to record...');
+  });
+
+  it('updates text if status changes to true', () => {
+    const wrapper = shallow(<RecordingStatusText currentRecordingStatus={'recording'} />);
+    expect(wrapper.find('.status').text()).toEqual('Recording...');
+  });
+});
