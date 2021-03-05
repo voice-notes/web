4382131a8dd5e548af0c82a3b448177daf40a8b1
Tim <timcpb@hotmail.com>
2021-03-05 12:02:43 +0000

    change isRecording prop to currentRecordingStatus  and update RecordingStatusText component name

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
+
 import { Title } from '../Title/title';
-import { RecordingStatus } from '../RecordingStatus/recordingStatus';
+import { RecordingStatusText } from '../RecordingStatusText/recordingStatusText';
 import styles from './header.module.css';
 import { ReactComponent as TapeLogo } from '../../assets/tape.svg';
-
-type RecordingStatus = 'ready' | 'recording' | 'recorded';
+import { RecordingStatus } from "../App/App";
 interface Props {
-  isRecording: RecordingStatus;
+  currentRecordingStatus: RecordingStatus;
 }
 
-export const Header = ({ isRecording }: Props) => {
+export const Header = ({ currentRecordingStatus }: Props) => {
   return (
     <div className={styles.header}>
       <TapeLogo className={styles.logo} title="Tape It logo" />
       <div className={styles.title}>
-        <Title isRecording={isRecording} />
-        <RecordingStatus isRecording={isRecording} />
+        <Title currentRecordingStatus={currentRecordingStatus} />
+        <RecordingStatusText currentRecordingStatus={currentRecordingStatus} />
       </div>
     </div>
   );
