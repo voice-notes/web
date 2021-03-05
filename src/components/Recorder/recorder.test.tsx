8f9891e27dd76aff2802f38767c54ba9a86ba709
Tim <timcpb@hotmail.com>
2021-03-05 12:04:14 +0000

    update Recorder props and remove test for Timer component

--- src/components/Recorder/recorder.test.tsx
+++ src/components/Recorder/recorder.test.tsx
@@ -1,28 +1,16 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
+import React from 'react';
 import { render } from '@testing-library/react';
 import { Recorder } from './recorder';
-import { Timer } from '../Timer';
 
 describe('Recorder', () => {
   it('renders App', () => {
-    const mockChangeRecordingStatus = jest.fn();
     const container = render(
       <Recorder
-        isRecording={false}
-        recordingStatus={'not recorded'}
-        onClickStatus={mockChangeRecordingStatus}
+        currentRecordingStatus={'ready'}
       />
     );
     expect(container).toMatchSnapshot();
   });
 });
 
-describe('Timer', () => {
-  it('is defaulted to 00:00', () => {
-    const wrapper = shallow(
-      <Timer isRecording={false} recordingStatus={'not recorded'}></Timer>
-    );
-    expect(wrapper.text()).toContain('00:00');
-  });
-});
+
