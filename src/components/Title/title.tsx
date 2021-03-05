752f9b4ea82e776250528324b027a2ced14e6fdc
Tim <timcpb@hotmail.com>
2021-03-05 12:08:46 +0000

    update isRecording to currentRecordingStatus

--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from './title.module.css';
 
-type RecordingStatus = 'ready' | 'recording' | 'recorded';
+import styles from './title.module.css';
+import { RecordingStatus } from "../App/App";
 interface Props {
-  isRecording: RecordingStatus;
+  currentRecordingStatus: RecordingStatus;
 }
 
-export const Title = ({ isRecording }: Props) => {
-  const text = isRecording === 'recording' ? 'Taping It' : 'Tape It';
+export const Title = ({ currentRecordingStatus }: Props) => {
+  const text = currentRecordingStatus === 'recording' ? 'Taping It' : 'Tape It';
   return <h1 className={styles.title}>{text}</h1>;
 };
