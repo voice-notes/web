ec2298f1e4ecfe3fbf1afacbecf591cf238ca7ca
Tim <timcpb@hotmail.com>
2021-03-05 15:31:37 +0000

    Alter test to reflect new slackId & responseUrl state properties

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -4,12 +4,14 @@ import { App } from './App';
 jest.mock('../Header/header');
 jest.mock('../Recorder/recorder');
 
+const baseResponseUrl = process.env.REACT_APP_BASE_RES_URL;
+
 beforeEach((): void => {
   window.history.pushState({}, 'Foo', '?&sender=sender&p1=one&p2=two&p3=three');
 });
 
 afterEach(() => {
-  jest.clearAllMocks();
+  jest.resetAllMocks();
 });
 
 test('renders App', () => {
@@ -18,14 +20,27 @@ test('renders App', () => {
 });
 
 test('captures and stores query params', () => {
-  const setQueryParams = jest.fn();
+  const setResponseUrl = jest.fn();
 
-  const useStateMock: any = (initState: any) => [initState, setQueryParams];
+  const useStateMock: any = (initState: any) => [initState, setResponseUrl];
   jest.spyOn(React, 'useState').mockImplementation(useStateMock);
 
-  const args = { p1: 'one', p2: 'two', p3: 'three', sender: 'sender' };
+  const mockResponseUrl = `${baseResponseUrl}/one/two/three`;
 
-  const app = render(<App />);
+  render(<App />);
 
-  expect(setQueryParams).toHaveBeenCalledWith(args);
+  expect(setResponseUrl).toHaveBeenCalledWith(mockResponseUrl);
+});
+
+test('captures and stores query params', () => {
+  const setSlackId = jest.fn();
+
+  const useStateMock: any = (initState: any) => [initState, setSlackId];
+  jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+
+  const mockSlackId = 'sender';
+
+  render(<App />);
+
+  expect(setSlackId).toHaveBeenCalledWith(mockSlackId);
 });
