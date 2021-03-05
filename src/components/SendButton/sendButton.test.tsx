c8b443821ad5553fe9d71adf18b86f782a64e111
Tim <timcpb@hotmail.com>
2021-03-05 12:07:54 +0000

    update isRecording to currentRecordingStatus and add test for different recordingStatus values

--- src/components/SendButton/sendButton.test.tsx
+++ src/components/SendButton/sendButton.test.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 import { SendButton } from './sendButton';
 
 describe('Button', () => {
   it('displays send button after recording', () => {
-    const wrapper = shallow(<SendButton recordingStatus={'recorded'} />);
+    const wrapper = shallow(<SendButton currentRecordingStatus={'recorded'} />);
     expect(wrapper.text()).toContain('send');
+  });
+
+  it('does not display send button before recording', () => {
+    const wrapper = shallow(<SendButton currentRecordingStatus={'ready'} />);
+    expect(wrapper.text()).not.toContain('send');
+  });
+
+  it('does not display send button during recording', () => {
+    const wrapper = shallow(<SendButton currentRecordingStatus={'recording'} />);
+    expect(wrapper.text()).not.toContain('send');
   });
 });
