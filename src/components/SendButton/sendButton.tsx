1cc5445ce8440fe63049ecd2faed58db329d5d9e
charlie-galb <galbraith.charlie@gmail.com>
2021-03-09 14:43:16 +0000

    Add mock api reqests to AWS cloud and backend

--- src/components/SendButton/sendButton.tsx
+++ src/components/SendButton/sendButton.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './sendButton.module.css';
 import { IconContext } from 'react-icons';
 import { FiArrowRight } from 'react-icons/fi';
+import axios from 'axios';
 
 import { RecordingStatus } from '../App/App';
+
 interface Props {
   currentRecordingStatus: RecordingStatus;
   slackId: string;
   responseUrl: string;
+  recordedBlob: any;
 }
 
-export const SendButton = ({ currentRecordingStatus }: Props) => {
+export const SendButton = ({ currentRecordingStatus, slackId, responseUrl, recordedBlob }: Props) => {
+
+  const [audioUrl, setAudioUrl] = useState<string >("");
+
+  const handleSend = async () => {
+    try {
+      await axios.post("AWS url", {blob: recordedBlob});
+      sendToBackEnd()
+    } catch(error) {
+      console.error(error);
+    }  
+  };
+
+  const sendToBackEnd = async () => {
+    await axios.post("backend url", {slackId: slackId, responseUrl: responseUrl, audioUrl: audioUrl})
+  };
+
+
   if (currentRecordingStatus === 'recorded') {
     return (
-      <button className={styles.button} id="send">
+      <button className={styles.button} id="send" onClick={handleSend}>
         <IconContext.Provider value={{ className: styles.icons }}>
           <FiArrowRight />
         </IconContext.Provider>
