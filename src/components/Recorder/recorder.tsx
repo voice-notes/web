7c4ef58e0c5c1cb11a1f8e6cb33d3aceccc288cb
charlie-galb <galbraith.charlie@gmail.com>
2021-03-09 14:42:09 +0000

    Add saveBlob callBack and rename currentRecordingStatusToggle to isRecording

--- src/components/Recorder/recorder.tsx
+++ src/components/Recorder/recorder.tsx
@@ -4,27 +4,30 @@ import { ReactMic } from 'react-mic';
 import { Timer } from '../Timer/timer';
 import styles from './recorder.module.css';
 import { RecordingStatus } from '../App/App';
+
 interface Props {
   currentRecordingStatus: RecordingStatus;
+  saveBlob: (blob: any) => void; 
 }
 
-export const Recorder = ({ currentRecordingStatus }: Props) => {
+export const Recorder = ({ currentRecordingStatus, saveBlob }: Props) => {
   function onData(recordedBlob: any) {
     console.log('chunk of real-time data is: ', recordedBlob);
   }
 
   function onStop(recordedBlob: any) {
+    saveBlob(recordedBlob)
     console.log('recordedBlob is: ', recordedBlob);
   }
 
-  function currentRecordingStatusToggle() {
+  function isRecording() {
     return currentRecordingStatus === 'recording';
   }
 
   return (
     <div className={styles.container}>
       <ReactMic
-        record={currentRecordingStatusToggle()}
+        record={isRecording()}
         onStop={onStop}
         onData={onData}
         strokeColor="#0A9E74"
