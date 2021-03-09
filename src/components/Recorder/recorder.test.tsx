7f0010628e9e48ee1d037c933e6433a4fa4da5b2
charlie-galb <galbraith.charlie@gmail.com>
2021-03-09 14:41:31 +0000

    Add saveBlob argument

--- src/components/Recorder/recorder.test.tsx
+++ src/components/Recorder/recorder.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Recorder } from './recorder';
+import { isJSDocEnumTag } from 'typescript';
 
 describe('Recorder', () => {
   it('renders App', () => {
-    const container = render(<Recorder currentRecordingStatus={'ready'} />);
+    const container = render(<Recorder currentRecordingStatus={'ready'} saveBlob={jest.fn()}/>);
     expect(container).toMatchSnapshot();
   });
 });
