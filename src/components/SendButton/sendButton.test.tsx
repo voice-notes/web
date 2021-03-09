e15f2b28d8e6b9e1f1b49160d66bc1095730d2b0
charlie-galb <galbraith.charlie@gmail.com>
2021-03-09 14:42:38 +0000

    Import axios and test API calls on send

--- src/components/SendButton/sendButton.test.tsx
+++ src/components/SendButton/sendButton.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import axios from 'axios';
+
 import { SendButton } from './sendButton';
 
 describe('Button', () => {
@@ -35,4 +37,30 @@ describe('Button', () => {
     );
     expect(wrapper.text()).not.toContain('send');
   });
+
+  it('does not display send button during recording', () => {
+    const wrapper = shallow(
+      <SendButton
+        currentRecordingStatus={'recording'}
+        slackId={''}
+        responseUrl={''}
+      />
+    );
+    expect(wrapper.text()).not.toContain('send');
+  });
+
+  it('sends a post request to AWS', async () => {
+    axios.post = jest.fn();
+    const wrapper = shallow(
+      <SendButton
+        currentRecordingStatus={'recorded'}
+        slackId={''}
+        responseUrl={''}
+        recordedBlob={''}
+      />
+    );
+    await wrapper.find("#send").simulate('click')
+    expect(axios.post).toHaveBeenCalledWith("AWS url", {blob: ''})
+    expect(axios.post).toHaveBeenCalledWith("backend url", {slackId: '', responseUrl: '', audioUrl: ''})
+  })
 });
