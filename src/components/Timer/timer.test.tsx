627cf9fcfb4f6715f64104605f95b5a7253ecbad
charlie-galb <galbraith.charlie@gmail.com>
2021-03-10 16:45:40 +0000

    Add test to check logic of timer

--- src/components/Timer/timer.test.tsx
+++ src/components/Timer/timer.test.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 
 import { Timer } from './timer';
 
 describe('Timer', () => {
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+  })
   it('is defaulted to 00:00', () => {
     const wrapper = shallow(<Timer currentRecordingStatus={'ready'}></Timer>);
     expect(wrapper.text()).toContain('00:00');
+  });
+  it('is defaulted to 00:00', async () => {
+    const {getByTestId} = render(<Timer currentRecordingStatus={'recording'}></Timer>);
+    jest.advanceTimersByTime(1000);
+    expect(getByTestId('timeDisplay').textContent).toContain('00:01');
+    jest.useRealTimers();
   });
 });
