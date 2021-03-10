d32a1179692c1e3cda08b432a11b163ef29cf7d9
charlie-galb <galbraith.charlie@gmail.com>
2021-03-10 16:43:28 +0000

    Move tests for buttonText to appropriate folder

--- src/components/RecordingButton/recordingButton.test.tsx
+++ src/components/RecordingButton/recordingButton.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { RecordingButton, RecordingButtonText } from './recordingButton';
+import { RecordingButton } from './recordingButton';
 
 describe('Button', () => {
   it('executes callback on click', () => {
@@ -15,35 +15,5 @@ describe('Button', () => {
     );
     wrapper.find('button').simulate('click');
     expect(mockToggleRecording).toHaveBeenCalled();
-  });
-});
-
-describe('Button Text', () => {
-  it('displays "start recording" if not recording', () => {
-    const wrapper = shallow(
-      <RecordingButtonText currentRecordingStatus={'ready'} />
-    );
-    expect(wrapper.text()).toContain('Start recording');
-  });
-
-  it('isRecording updates the button text', () => {
-    const wrapper = shallow(
-      <RecordingButtonText currentRecordingStatus={'recording'} />
-    );
-    expect(wrapper.text()).toContain('Stop recording');
-  });
-
-  it('displays rerecord message after the recording is taken', () => {
-    const wrapper = shallow(
-      <RecordingButtonText currentRecordingStatus={'recorded'} />
-    );
-    expect(wrapper.text()).toContain('Re-record');
-  });
-
-  it('restarts the recording message after re-recording', () => {
-    const wrapper = shallow(
-      <RecordingButtonText currentRecordingStatus={'recording'} />
-    );
-    expect(wrapper.text()).toContain('Stop recording');
   });
 });
