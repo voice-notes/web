bc87fd364984a6403bed8a137c51290cf1f1bf0e
Tim <timcpb@hotmail.com>
2021-03-11 15:09:50 +0000

    Add extra line

--- src/components/RecordingButtonText/recordingButtonText.tsx
+++ src/components/RecordingButtonText/recordingButtonText.tsx
@@ -4,13 +4,13 @@ import styles from './recordingButtonText.module.css';
 import { RecordingStatus } from '../App/App';
 
 interface Props {
-    currentRecordingStatus: RecordingStatus;
-  }
+  currentRecordingStatus: RecordingStatus;
+}
 
 export const RecordingButtonText = ({ currentRecordingStatus }: Props) => {
-    const mediaAction = currentRecordingStatus === 'recording' ? 'Stop' : 'Start';
-    if (currentRecordingStatus === 'recorded') {
-      return <span className={styles.text}>Re-record</span>;
-    }
-    return <span className={styles.text}>{mediaAction} recording</span>;
+  const mediaAction = currentRecordingStatus === 'recording' ? 'Stop' : 'Start';
+  if (currentRecordingStatus === 'recorded') {
+    return <span className={styles.text}>Re-record</span>;
+  }
+  return <span className={styles.text}>{mediaAction} recording</span>;
 };
