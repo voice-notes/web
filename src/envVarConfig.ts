0c3369105fd44e25a7d1bafb04f9eddfead633a4
Tim <timcpb@hotmail.com>
2021-03-11 15:12:08 +0000

    linting

--- src/envVarConfig.ts
+++ src/envVarConfig.ts
@@ -1,1 +1,4 @@
-export const { REACT_APP_BACKEND_GRAPHQL_ENDPOINT, REACT_APP_BASE_RES_URL  } = process.env
+export const {
+  REACT_APP_BACKEND_GRAPHQL_ENDPOINT,
+  REACT_APP_BASE_RES_URL,
+} = process.env;
