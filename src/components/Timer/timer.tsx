aac31954b5b39ef988c60c05b3f8ad48935c4ed6
charlie-galb <galbraith.charlie@gmail.com>
2021-03-11 15:51:11 +0000

    Add resetTimer() and hasReset boolean

--- src/components/Timer/timer.tsx
+++ src/components/Timer/timer.tsx
@@ -10,29 +10,28 @@ interface Props {
 export const Timer = ({ currentRecordingStatus, recordingExists }: Props) => {
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const [hasReset, setHasReset] = useState<boolean>(false);
 
   const formatValue = (value: number) => `${value < 10 ? `0${value}` : value}`;
 
-  // useEffect(() => {
-  //   const resetTime = () => {
-  //     setSeconds(0);
-  //     setMinutes(0);
-  //   };
-  //   if (currentRecordingStatus === 'recording' && recordingExists === true) {
-  //     resetTime();
-  //   }
-  // }, [recordingExists]);
+  const resetTimer = () => {
+    setSeconds(0);
+    setMinutes(0);
+  };
 
   useEffect(() => {
     let interval: any;
     if (currentRecordingStatus === 'recording') {
-      setSeconds(0);
-      setMinutes(0);
+      if (recordingExists && !hasReset) {
+        resetTimer();
+        setHasReset(true);
+      }
       interval = setInterval(() => {
         setTime();
       }, 1000);
     } else if (currentRecordingStatus === 'recorded' && seconds !== 0) {
       clearInterval(interval);
+      setHasReset(false);
     }
     const setTime = () => {
       setSeconds((seconds) => seconds + 1);
@@ -43,7 +42,7 @@ export const Timer = ({ currentRecordingStatus, recordingExists }: Props) => {
     };
 
     return () => clearInterval(interval);
-  }, [currentRecordingStatus, seconds]);
+  }, [currentRecordingStatus, seconds, hasReset, recordingExists]);
 
   return (
     <div data-testid="timeDisplay" className={styles.timer}>
