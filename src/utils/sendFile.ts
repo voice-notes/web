067b315f5a402e2afd35b726cd02c9263e6e85f9
Tim <timcpb@hotmail.com>
2021-03-17 16:19:30 +0000

    convert to ts and export sendFile function

---
+++ src/utils/sendFile.ts
@@ -0,0 +1,33 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+import {
+  REACT_APP_AWS_ENDPOINT,
+  REACT_APP_AWS_ACCESS_KEY_ID,
+  REACT_APP_AWS_SECRET_ACCESS_KEY,
+  REACT_APP_BUCKET_NAME,
+} from '../envVarConfig';
+
+const loginParams = {
+  endpoint: `${REACT_APP_AWS_ENDPOINT}`,
+  accessKeyId: `${REACT_APP_AWS_ACCESS_KEY_ID}`,
+  secretAccessKey: `${REACT_APP_AWS_SECRET_ACCESS_KEY}`,
+  s3BucketEndpoint: true,
+};
+
+const uploadParams = {
+  Bucket: `${REACT_APP_BUCKET_NAME}`,
+  Key: 'Test',
+  Body: 'this is the body',
+};
+
+const client = new S3Client({ credentials: loginParams, region: 'eu-west-2' });
+
+export const sendFile = async () => {
+  try {
+    const file = new PutObjectCommand(uploadParams);
+    const data = await client.send(file);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
