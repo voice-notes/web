6871a32c3e0e5fe4bbd644d3fbf627671f88cbcd
Tim <timcpb@hotmail.com>
2021-03-24 15:23:20 +0000

    IMport region as env_var

--- src/utils/sendFile.ts
+++ src/utils/sendFile.ts
@@ -5,6 +5,7 @@ import {
   REACT_APP_AWS_ACCESS_KEY_ID,
   REACT_APP_AWS_SECRET_ACCESS_KEY,
   REACT_APP_BUCKET_NAME,
+  REACT_APP_AWS_REGION,
 } from '../envVarConfig';
 
 const loginParams = {
@@ -14,14 +15,16 @@ const loginParams = {
   s3BucketEndpoint: true,
 };
 
-const client = new S3Client({ credentials: loginParams, region: 'eu-west-2' });
+const client = new S3Client({
+  credentials: loginParams,
+  region: REACT_APP_AWS_REGION,
+});
 
 export const sendFile = async (file: Blob, slackId: string) => {
-
   const uploadParams = {
     Bucket: `${REACT_APP_BUCKET_NAME}`,
     Key: `${slackId}@${Date.now()}`,
-    Body: file
+    Body: file,
   };
 
   try {
