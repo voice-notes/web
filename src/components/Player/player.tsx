c39cc22f8465085d05718a2845bf5b8e9a758b9d
charlie-galb <galbraith.charlie@gmail.com>
2021-03-25 17:37:44 +0000

    Linting: indentation

--- src/components/Player/player.tsx
+++ src/components/Player/player.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactPlayer from 'react-player';
 
 interface Props {
-    url: string
-    setIsPlaying: (arg: boolean) => void
-    isPlaying: boolean;
-  }
-
-export const Player = ({
-    isPlaying,
-    url,
-    setIsPlaying
-  }: Props) => {
-
-    return (
-        <div>
-            <ReactPlayer url={url} playing={isPlaying} onEnded={() => setIsPlaying(false)}/>
-        </div>
-    )
+  url: string;
+  setIsPlaying: (arg: boolean) => void;
+  isPlaying: boolean;
 }
+
+export const Player = ({ isPlaying, url, setIsPlaying }: Props) => {
+  return (
+    <div>
+      <ReactPlayer
+        url={url}
+        playing={isPlaying}
+        onEnded={() => setIsPlaying(false)}
+      />
+    </div>
+  );
+};
