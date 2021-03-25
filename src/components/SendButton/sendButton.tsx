d3f50337951c983ad859375fe39ea03d7acb36df
charlie-galb <galbraith.charlie@gmail.com>
2021-03-25 16:40:44 +0000

    Add sendToBackEnd() back in with updated params

--- src/components/SendButton/sendButton.tsx
+++ src/components/SendButton/sendButton.tsx
@@ -4,7 +4,7 @@ import { IconContext } from 'react-icons';
 import { FiArrowRight } from 'react-icons/fi';
 import axios from 'axios';
 
-import { REACT_APP_BACKEND_GRAPHQL_ENDPOINT } from '../../envVarConfig';
+import { REACT_APP_BACKEND_GRAPHQL_ENDPOINT, REACT_APP_AWS_ENDPOINT } from '../../envVarConfig';
 import { sendFile } from '../../utils/sendFile';
 import { RecordingStatus } from '../App/App';
 
@@ -21,7 +21,7 @@ export const SendButton = ({
   responseUrl,
   recordedBlob,
 }: Props) => {
-  // const [audioUrl, setAudioUrl] = useState<string>('');
+  const [audioUrl, setAudioUrl] = useState<string>('');
 
   const handleSend = async () => {
     try {
@@ -32,27 +32,30 @@ export const SendButton = ({
   };
 
   const sendToAWS = async () => {
+    const keyName = `${slackId}@${Date.now()}.wav`
+    const s3ObjectUrl = `${REACT_APP_AWS_ENDPOINT + "/" + keyName}`
     try {
-      sendFile(recordedBlob, slackId);
-      // const { url } = response.data;
-      // setAudioUrl(url);
+      await sendFile(recordedBlob, slackId, keyName);
+      setAudioUrl(s3ObjectUrl)
+      
     } catch (error) {
       console.error(error);
     }
   };
 
-  // useEffect(() => {
-  //   const sendToBackEnd = async () => {
-  //     await axios.post(`${REACT_APP_BACKEND_GRAPHQL_ENDPOINT}`, {
-  //       slackId: slackId,
-  //       responseUrl: responseUrl,
-  //       audioUrl: audioUrl,
-  //     });
-  //   };
-  //   if (audioUrl !== '') {
-  //     sendToBackEnd();
-  //   }
-  // }, [audioUrl, slackId, responseUrl]);
+  useEffect(() => {
+    console.log(audioUrl)
+    const sendToBackEnd = async () => {
+      await axios.post(`${REACT_APP_BACKEND_GRAPHQL_ENDPOINT}`, {
+       query: `mutation {
+        createNote(slackID: "${slackId}", responseUrl: "${responseUrl}", audioUrl: "${audioUrl}"){responseUrl}
+      }`
+    })
+  }
+    if (audioUrl !== '') {
+      sendToBackEnd();
+    }
+  }, [audioUrl, slackId, responseUrl]);
 
   if (currentRecordingStatus === 'recorded') {
     return (
