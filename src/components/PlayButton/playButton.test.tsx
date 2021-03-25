11b7747f229fd2d724b09c724b8052ced033b912
charlie-galb <galbraith.charlie@gmail.com>
2021-03-25 17:37:21 +0000

    Linting: indentation

--- src/components/PlayButton/playButton.test.tsx
+++ src/components/PlayButton/playButton.test.tsx
@@ -4,33 +4,30 @@ import { shallow } from 'enzyme';
 import { PlayButton } from './playButton';
 
 describe('Button', () => {
-    it('displays play button after recording', () => {
-      const wrapper = shallow(
-        <PlayButton
-          currentRecordingStatus={'recorded'}
-          setIsPlaying={jest.fn()}
-        />
-      );
-      expect(wrapper.text()).toContain('play');
-    });
-  
-    it('does not display play button before recording', () => {
-      const wrapper = shallow(
-        <PlayButton
-          currentRecordingStatus={'ready'}
-          setIsPlaying={jest.fn()}
-        />
-      );
-      expect(wrapper.text()).not.toContain('play');
-    });
-  
-    it('does not display play button during recording', () => {
-      const wrapper = shallow(
-        <PlayButton
-          currentRecordingStatus={'recording'}
-          setIsPlaying={jest.fn()}
-        />
-      );
-      expect(wrapper.text()).not.toContain('play');
-    });
+  it('displays play button after recording', () => {
+    const wrapper = shallow(
+      <PlayButton
+        currentRecordingStatus={'recorded'}
+        setIsPlaying={jest.fn()}
+      />
+    );
+    expect(wrapper.text()).toContain('play');
+  });
+
+  it('does not display play button before recording', () => {
+    const wrapper = shallow(
+      <PlayButton currentRecordingStatus={'ready'} setIsPlaying={jest.fn()} />
+    );
+    expect(wrapper.text()).not.toContain('play');
+  });
+
+  it('does not display play button during recording', () => {
+    const wrapper = shallow(
+      <PlayButton
+        currentRecordingStatus={'recording'}
+        setIsPlaying={jest.fn()}
+      />
+    );
+    expect(wrapper.text()).not.toContain('play');
+  });
 });
