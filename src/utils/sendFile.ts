3ee7a1e44939ac7268bf6e6c111d75873fb02cfe
charlie-galb <galbraith.charlie@gmail.com>
2021-03-25 16:41:08 +0000

    Extract keyName into variable in sendButton

--- src/utils/sendFile.ts
+++ src/utils/sendFile.ts
@@ -20,11 +20,11 @@ const client = new S3Client({
   region: REACT_APP_AWS_REGION,
 });
 
-export const sendFile = async (file: any, slackId: string) => {
+export const sendFile = async (file: any, slackId: string, keyName: string) => {
   console.log(slackId);
   const uploadParams = {
     Bucket: `${REACT_APP_BUCKET_NAME}`,
-    Key: `${slackId}@${Date.now()}.wav`,
+    Key: keyName,
     Body: file.blob,
     ContentType: 'audio/wav',
     ACL: 'public-read',
