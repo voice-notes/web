fd5af0655d7c238f8f54244163ca186aaf8c121b
Tim <timcpb@hotmail.com>
2021-04-01 15:49:02 +0100

    Rename currentRecordingStatus to recordingStatus

--- src/components/RecordingStatusText/recordingStatusText.tsx
+++ src/components/RecordingStatusText/recordingStatusText.tsx
@@ -4,11 +4,11 @@ import styles from './recordingStatusText.module.css';
 import { RecordingStatus } from '../App/App';
 
 interface Props {
-  currentRecordingStatus: RecordingStatus;
+  recordingStatus: RecordingStatus;
 }
 
-export const RecordingStatusText = ({ currentRecordingStatus }: Props) => {
-  if (currentRecordingStatus === 'recording') {
+export const RecordingStatusText = ({ recordingStatus }: Props) => {
+  if (recordingStatus === 'recording') {
     return <div className={styles.status}>Recording...</div>;
   }
   return <div className={styles.status}>Ready to record...</div>;
