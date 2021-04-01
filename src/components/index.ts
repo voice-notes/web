99ce9e50bc3b22bc717805fc10d879a735cfd44d
Tim <timcpb@hotmail.com>
2021-04-01 15:39:30 +0100

    create new copmonents/index file to export components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { Player } from './Player/player';
+import { Header } from './Header/header';
+import { Recorder } from './Recorder/recorder';
+import { RecordingButton } from './RecordingButton/recordingButton';
+import { SendButton } from './SendButton/sendButton';
+import { PlayButton } from './PlayButton/playButton';
+
