c8d9d2be2a7cf6ead909ce5701e534c96dce4752
Tim <timcpb@hotmail.com>
2021-04-01 15:50:25 +0100

    Rename currentRecordingStatus to recordingStatus

--- src/components/Title/title.test.tsx
+++ src/components/Title/title.test.tsx
@@ -5,12 +5,12 @@ import { Title } from './title';
 
 describe('Title', () => {
   it('displays "Tape It" when not recording', () => {
-    const wrapper = shallow(<Title currentRecordingStatus={'ready'} />);
+    const wrapper = shallow(<Title recordingStatus={'ready'} />);
     expect(wrapper.text()).toEqual('Tape It');
   });
 
   it('displays "Taping It" when recording', () => {
-    const wrapper = shallow(<Title currentRecordingStatus={'recording'} />);
+    const wrapper = shallow(<Title recordingStatus={'recording'} />);
     expect(wrapper.text()).toEqual('Taping It');
   });
 });
