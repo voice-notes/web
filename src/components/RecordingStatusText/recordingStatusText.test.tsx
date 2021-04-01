38967dca8d310cc96940a6b1d910a8425a82e6a1
Tim <timcpb@hotmail.com>
2021-04-01 15:49:09 +0100

    Rename currentRecordingStatus to recordingStatus

--- src/components/RecordingStatusText/recordingStatusText.test.tsx
+++ src/components/RecordingStatusText/recordingStatusText.test.tsx
@@ -4,15 +4,13 @@ import { RecordingStatusText } from './recordingStatusText';
 
 describe('RecordingStatusText', () => {
   it('renders the current recording status', () => {
-    const wrapper = shallow(
-      <RecordingStatusText currentRecordingStatus={'ready'} />
-    );
+    const wrapper = shallow(<RecordingStatusText recordingStatus={'ready'} />);
     expect(wrapper.find('.status').text()).toEqual('Ready to record...');
   });
 
   it('updates text if status changes to true', () => {
     const wrapper = shallow(
-      <RecordingStatusText currentRecordingStatus={'recording'} />
+      <RecordingStatusText recordingStatus={'recording'} />
     );
     expect(wrapper.find('.status').text()).toEqual('Recording...');
   });
