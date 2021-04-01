7d12af39e8f44b765f02b08e08a78a5f95f40589
Tim <timcpb@hotmail.com>
2021-04-01 15:44:05 +0100

    Rename currentRecordingStatus to recordingStatus

--- src/components/PlayButton/playButton.test.tsx
+++ src/components/PlayButton/playButton.test.tsx
@@ -6,27 +6,21 @@ import { PlayButton } from './playButton';
 describe('Button', () => {
   it('displays play button after recording', () => {
     const wrapper = shallow(
-      <PlayButton
-        currentRecordingStatus={'recorded'}
-        setIsPlaying={jest.fn()}
-      />
+      <PlayButton recordingStatus={'recorded'} setIsPlaying={jest.fn()} />
     );
     expect(wrapper.text()).toContain('play');
   });
 
   it('does not display play button before recording', () => {
     const wrapper = shallow(
-      <PlayButton currentRecordingStatus={'ready'} setIsPlaying={jest.fn()} />
+      <PlayButton recordingStatus={'ready'} setIsPlaying={jest.fn()} />
     );
     expect(wrapper.text()).not.toContain('play');
   });
 
   it('does not display play button during recording', () => {
     const wrapper = shallow(
-      <PlayButton
-        currentRecordingStatus={'recording'}
-        setIsPlaying={jest.fn()}
-      />
+      <PlayButton recordingStatus={'recording'} setIsPlaying={jest.fn()} />
     );
     expect(wrapper.text()).not.toContain('play');
   });
