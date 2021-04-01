9eae90cfb2b59813f142efd953dd9714fec6d97c
Tim <timcpb@hotmail.com>
2021-04-01 15:48:05 +0100

    Add tests for recording button icons and text

--- src/components/RecordingButton/recordingButton.test.tsx
+++ src/components/RecordingButton/recordingButton.test.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { RecordingButton } from './recordingButton';
+import { TiMediaRecordOutline, TiMediaStopOutline } from 'react-icons/ti';
 
 describe('Button', () => {
+  const mockToggleRecording = jest.fn();
+
   it('executes callback on click', () => {
-    const mockToggleRecording = jest.fn();
     const wrapper = shallow(
       <RecordingButton
-        currentRecordingStatus={'ready'}
+        recordingStatus={'ready'}
+        onClickRecord={() => {
+          mockToggleRecording();
+        }}
+      />
+    );
+    expect(wrapper.containsMatchingElement(<TiMediaRecordOutline />)).toEqual(
+      true
+    );
+    expect(wrapper.text()).toContain('Start recording');
+  });
+
+  it('renders correctly when recording status is recording', () => {
+    const wrapper = shallow(
+      <RecordingButton
+        recordingStatus={'recording'}
+        onClickRecord={() => {
+          mockToggleRecording();
+        }}
+      />
+    );
+    expect(wrapper.containsMatchingElement(<TiMediaStopOutline />)).toEqual(
+      true
+    );
+    expect(wrapper.text()).toContain('Stop recording');
+  });
+
+  it('renders correctly when recording status is not recording', () => {
+    const wrapper = shallow(
+      <RecordingButton
+        recordingStatus={'recorded'}
+        onClickRecord={() => {
+          mockToggleRecording();
+        }}
+      />
+    );
+    expect(wrapper.containsMatchingElement(<TiMediaRecordOutline />)).toEqual(
+      true
+    );
+    expect(wrapper.text()).toContain('Re-record');
+  });
+
+  it('executes callback on click', () => {
+    const wrapper = shallow(
+      <RecordingButton
+        recordingStatus={'ready'}
         onClickRecord={() => {
           mockToggleRecording();
         }}
