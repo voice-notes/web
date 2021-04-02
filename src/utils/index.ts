d5ada4eeb4da37ccc136e4f626b238e2c4f435bd
Tim <timcpb@hotmail.com>
2021-04-02 14:53:47 +0100

    import all utils functions and export as one object

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { returnParsedParams } from './returnParsedParams';
+import { sendFileToCloud } from './sendFileToCloud';
+import { sendToBackEnd } from './sendToBackEnd';
+
+export { returnParsedParams, sendFileToCloud, sendToBackEnd };
